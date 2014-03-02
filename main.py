3e41772208317b8149161bce9598b09210f08774
c9zacabo <c9zacabo@gmail.com>
2014-03-02 23:06:06 +0800

    Update main.py
    Explore the World!

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello Albert!')
+        self.response.write('Hello Albert and Andy!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
